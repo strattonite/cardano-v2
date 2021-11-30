4d961d25860c182ca0c8ae717bbd15fd7c215ec6
strattonite <coetzeetrist@gmail.com>
2021-11-30 16:49:48 +0000

    finished

--- management/create-wallets.js
+++ management/create-wallets.js
@@ -1,58 +1,70 @@
 const bip = require("bip39");
-const {randomBytes} = require("crypto");
+const { randomBytes } = require("crypto");
+const col = require("./db").db("cardano").collection("wallets");
 const serialization = require("@emurgo/cardano-serialization-lib-nodejs");
 
-module.exports = {
-    import: (mnemonic)=>{
-        const ent = bip.mnemonicToEntropy(mnemonic);
-        return {
-            ...fromEntropy(ent),
-            mnemonic
-        }
-    },
-    create: ()=>{
-        const {mnemonic, entropy} = getEntropy();
-        return {
-            ...fromEntropy(entropy),
-            mnemonic
-        }
-    }
-}
+/**
+ * @param {Object} conf
+ * @param {string} conf.name
+ * @param {string} [conf.mnemonic]
+ * @returns
+ */
+module.exports = async ({ name, mnemonic }) => {
+  let entropy;
+  mnemonic
+    ? (entropy = bip.mnemonicToEntropy(mnemonic))
+    : ({ mnemonic, entropy } = getEntropy());
+
+  return await getWallet({
+    ...fromEntropy(entropy),
+    mnemonic,
+    name,
+  });
+};
 
 const harden = (num) => 0x80000000 + num;
 
-const getEntropy = ()=>{
-    const entropy = randomBytes(32);
-    const mnemonic = bip.entropyToMnemonic(entropy);
-    return {
-        entropy,
-        mnemonic
-    }
-}
+const getEntropy = () => {
+  const entropy = randomBytes(32);
+  const mnemonic = bip.entropyToMnemonic(entropy);
+  return {
+    entropy,
+    mnemonic,
+  };
+};
 
-const fromEntropy = (ent)=>{
-    const key = serialization.Bip32PrivateKey.from_bip39_entropy(ent, Buffer.from(""));
-    const account = key
+const fromEntropy = (ent) => {
+  const key = serialization.Bip32PrivateKey.from_bip39_entropy(
+    ent,
+    Buffer.from("")
+  );
+  const account = key
     .derive(harden(1852))
     .derive(harden(1815))
     .derive(harden(0));
 
-    const utxoPub = account.derive(0).derive(0).to_public();
-    const stake = account.derive(2).derive(0).to_public();
+  const utxoPub = account.derive(0).derive(0).to_public();
+  const stake = account.derive(2).derive(0).to_public();
 
-    const address = serialization.BaseAddress.new(
-        serialization.NetworkInfo.mainnet().network_id(),
-        serialization.StakeCredential.from_keyhash(utxoPub.to_raw_key().hash()),
-        serialization.StakeCredential.from_keyhash(stake.to_raw_key().hash()),
-    )
+  const address = serialization.BaseAddress.new(
+    serialization.NetworkInfo.mainnet().network_id(),
+    serialization.StakeCredential.from_keyhash(utxoPub.to_raw_key().hash()),
+    serialization.StakeCredential.from_keyhash(stake.to_raw_key().hash())
+  );
 
-    console.log(address.to_address().to_bech32("addr"))
+  return {
+    key,
+    address,
+  };
+};
 
-    return {
-        key,
-        account,
-        utxoPub,
-        stake,
-        address
-    }
-}
+const getWallet = async (raw) => {
+  const w = {
+    ...raw,
+    balance: 0,
+    assets: [],
+    utxos: [],
+  };
+  await col.insertOne(w);
+  return w;
+};
