f53dd7e92ca4f6a10a3d7739fafed7f913488fa4
strattonite <coetzeetrist@gmail.com>
2021-12-07 14:33:14 +0000

    commit

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
-const create = require("./management/create-wallets");
+process.env.NODE_ENV == "test";
+require("dotenv").config();
 
-create({ name: "test" }).then((wallet) => console.log(wallet));
+const create = require("./management/create-wallets");
+const { poll, pollTest } = require("./management/poll");
+const conf = require("./test-conf.json");
+const db = require("./management/db");
+const build = require("./transactions/build");
+const submit = require("./transactions/submit");
+
+db.once("connectionReady", async () => {
+  const col = db.db("cardano").collection("wallets");
+  let test1 = await col.findOne({ name: "test1" });
+  let test2 = await col.findOne({ name: "test2" });
+  test1 = await pollTest(test1);
+  test2 = await pollTest(test2);
+  //   const payments = [
+  //     {
+  //       address: test2.address,
+  //       amount: 2000000,
+  //     },
+  //   ];
+  //   const built = build(payments, test1);
+  //   const sum = await submit(built);
+  //   console.log(sum);
+});
